6b09cc3b89d402c578d4e4e42eb718d6a1600614
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-07-19 21:49:06 +0600

    file-route-structure-env-shadcn-intiation

---
+++ app/(authGroup)/_components/LogInButton.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { Button } from '@base-ui/react'
+import React from 'react'
+
+const LogInButton = () => {
+    console.log(process.env.BACKEND_API_URL,"Hello");
+    console.log(process.env.NEXT_PUBLIC_BACKEND_API_URL,"Hello");
+    
+  return (
+    <Button>
+        Log In
+    </Button>
+  )
+}
+
+export default LogInButton
---
+++ app/(publicGroup)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>Hello
+      <Button
+      size={"xs"}
+      variant={"destructive"}
+      >Click me</Button>
+    </div>
+  );
+}
