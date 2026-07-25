8a019a96628867fede7fd13e82cd4d18534648a9
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-07-26 00:12:26 +0600

    Getting New Access Token Using Refresh Token In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@ import { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtUtils } from "./utils/jwt";
+import { getNewAccessToken } from "./service/refreshToken";
 
 const AUTH_ROUTES = ["/login", "/register"];
 const PUBLIC_ROUTES = ["/", "/news"];
@@ -11,25 +12,55 @@ const PUBLIC_ROUTES = ["/", "/news"];
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  const cookieStore =  await cookies()
+  const cookieStore = await cookies();
   // const accessToken = cookieStore.get("accessToken")?.value;
 
-  const accessToken = request.cookies.get("accessToken")?.value;
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  const decodedToken = accessToken
+  let decodedAccessToken = accessToken
     ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
     : null;
 
-  let userRole = null;
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
 
-  if (!decodedToken?.success) {
-    // token has expired or is invalid. clear the cookie
-    cookieStore.delete("accessToken");
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    // access token has expired but refresh token is valid, get new access token
+    const result = await getNewAccessToken();
+    console.log(result)
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+
+      decodedAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
   }
 
-  if (decodedToken?.success && decodedToken.data) {
-    userRole = (decodedToken.data as JwtPayload).role;
+  let userRole = null;
+
+  if (!decodedAccessToken?.success) {
+    // token has expired or is invalid. clear the cookie
+    cookieStore.delete("accessToken");
+    // return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (decodedAccessToken?.success && decodedAccessToken.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
   }
 
   //user is not logged in and trying to access login or register page, redirect to dashboard
