dde3c852be179ccdc3be5b9a07cc3687195425ef
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-07-25 22:58:31 +0600

    Handling Auth Routes In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,39 @@
+// import { cookies } from "next/headers";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const AUTH_ROUTES = ["/login","/register"]
+
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  console.log(request, "request");
-  console.log("proxy");
+export async function proxy(request: NextRequest) {
+
+  const pathname = request.nextUrl.pathname;
+
+  // const cookieStore =  await cookies()
+  // const accessToken = cookieStore.get("accessToken")?.value;
+
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodedToken = accessToken? jwt.decode(accessToken) as JwtPayload : null;
+
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+
+  //user is not logged in and trying to access login or register page, redirect to dashboard
+  if(accessToken && AUTH_ROUTES.includes(pathname)){
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+  else if(userRole === "ADMIN"){
+    return NextResponse.redirect(new URL('/admin-dashboard', request.url))
+  }
+  else if(userRole === "AUTHOR"){
+    return NextResponse.redirect(new URL('/author-dashboard', request.url))
+  }
+
   //   return NextResponse.redirect(new URL('/', request.url))
   return NextResponse.next();
 }
