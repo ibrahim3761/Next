964b14e6f53ded48a08811620e61855fa74389b4
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-07-25 23:07:14 +0600

    Authenticating Protected Routes In Proxy

--- proxy.ts
+++ proxy.ts
@@ -3,11 +3,11 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const AUTH_ROUTES = ["/login","/register"]
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/news"];
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
-
   const pathname = request.nextUrl.pathname;
 
   // const cookieStore =  await cookies()
@@ -15,7 +15,9 @@ export async function proxy(request: NextRequest) {
 
   const accessToken = request.cookies.get("accessToken")?.value;
 
-  const decodedToken = accessToken? jwt.decode(accessToken) as JwtPayload : null;
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
 
   let userRole = null;
 
@@ -24,14 +26,29 @@ export async function proxy(request: NextRequest) {
   }
 
   //user is not logged in and trying to access login or register page, redirect to dashboard
-  if(accessToken && AUTH_ROUTES.includes(pathname)){
-    return NextResponse.redirect(new URL('/dashboard', request.url))
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
-  else if(userRole === "ADMIN"){
-    return NextResponse.redirect(new URL('/admin-dashboard', request.url))
-  }
-  else if(userRole === "AUTHOR"){
-    return NextResponse.redirect(new URL('/author-dashboard', request.url))
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  // Authenticated pages are accessible only if the user is logged in, otherwise redirect to login page
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   //   return NextResponse.redirect(new URL('/', request.url))
